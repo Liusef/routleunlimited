4efc6e8d34490173709eda94179a14cc39952567
quacksire <sam@quacksire.dev>
2025-03-24 01:33:09 -0700

    add more kofi

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,17 +7,24 @@ import Link from "next/link"
  */
 export function Footer() {
     return (
-        <footer className="bg-zinc-900 text-zinc-400 w-full content-center">
-            <div className="container mx-auto px-4 mb-5 bottom-0 ">
-                <div className="flex justify-between items-center space-y-4 ">
-                    <div className="text-center sm:text-left">
+        <footer className="bg-zinc-900 text-zinc-400 w-screen">
+            <div className="container mx-auto px-4 mb-5 bottom-0 w-screen">
+                <div className="flex items-center flex-wrap justify-evenly">
+                    <div className="text-center">
                         <Link
                             href="https://bsky.app/profile/quacksire.dev"
                             target="_blank"
-                            rel="noopener noreferrer"
                             className="hover:text-primary transition-colors"
                         >
-                            Made with ❤️ in 🌉 by quacksire
+                            Made with ❤️ in 🌉 by quacksire ↗
+                        </Link>
+                    </div>
+                    <div className="text-center">
+                        <Link
+                            href="https://ko-fi.com/quacksire"
+                            target="_blank"
+                        >
+                           Buy me a coffee ☕ ↗
                         </Link>
                     </div>
                 </div>
