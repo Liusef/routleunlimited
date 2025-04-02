b18a3643567b127163e51241dba37e6a86a5092a
quacksire <sam@quacksire.dev>
2025-04-01 18:57:45 -0700

    uta

--- lib/transit.ts
+++ lib/transit.ts
@@ -18,6 +18,7 @@ import * as bart from "@/lib/transit/o-9q9-bart.json";
 import * as bayferry from "@/lib/transit/o-9q9p-sanfranciscobayferry.json";
 import * as sacrt from "@/lib/transit/o-9qce-sacramentoregionaltransit.json";
 import * as omni from "@/lib/transit/o-9qh-omnitrans.json";
+import * as uta from "@/lib/transit/o-9x0-utahtransitauthority.json";
 
 
 import {RoutesResponse} from "@/lib/types/transitland";
@@ -44,7 +45,8 @@ export const agencyMap: Record<string, RoutesResponse> = {
     "o-9q9-bart": bart,
     "o-9q9p-sanfranciscobayferry": bayferry,
     "o-9qce-sacramentoregionaltransit": sacrt,
-    "o-9qh-omnitrans": omni
+    "o-9qh-omnitrans": omni,
+    "o-9x0-utahtransitauthority": uta
 
     //'o-dqc-met': dc
 }
@@ -78,6 +80,7 @@ export const agencies: AgencyCard[] = [
     { title: "San Francisco Bay Ferry", subtitle: "San Francisco Bay Area", image: "/placeholder.svg", onestop: "o-9q9p-sanfranciscobayferry", available: false },
     { title: "Sacramento Regional Transit", subtitle: "Sacramento, CA", image: "/placeholder.svg", onestop: "o-9qce-sacramentoregionaltransit", available: false },
     { title: "Omnitrans", subtitle: "San Bernardino County, CA", image: "/placeholder.svg", onestop: "o-9qh-omnitrans", available: false },
+    { title: "Utah Transit Authority", subtitle: "Salt Lake City, UT", image: "/placeholder.svg", onestop: "o-9x0-utahtransitauthority", available: false },
 
     //{ title: "WMATA", subtitle: "Washington DC", image: "/placeholder.svg", onestop: "o-dqc-met", available: true },
 ]
